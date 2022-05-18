296666f077c6ebdfc2ff9ec3f81062599e57daa2
Marcelo8819 <silva.marcelohg@gmail.com>
2022-05-18 07:48:29 -0300

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import * as C from './App.styles'
+import {useState} from 'react';
+import * as C from './App.styles';
+import {Item} from './types/item';
+
 
 const App = () => {
+    const [list,setlist] = useState<[]>([
+        {id: 1, name: 'Comprar o pão na padaria', done:false},
+        {id:2, name:'Comprar bolo na padaria',done:false},
+    ]);
     return (
         <C.Container>
          <C.Area>
          <C.Header>Lista de Tarefas</C.Header>
+
+        {list.map((item, index)=>(
+            <div>{item.name}</div>
+        ))}
          </C.Area>
         </C.Container>
     );
